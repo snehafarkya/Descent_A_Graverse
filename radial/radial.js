89762e5ddac218e4f7b4dfebc5f2436ee1e5c055
snehafarkya <snhafarkya@gmail.com>
2021-11-26 21:13:34 +0530

    initial commitment

---
+++ radial/radial.js
@@ -0,0 +1,88 @@
+AOS.init();
+
+function click1() {
+  document.getElementById("cc1");
+  cc1.select();
+  document.execCommand("copy")
+}
+function click2() {
+  document.getElementById("cc2");
+  cc2.select();
+  document.execCommand("copy")
+}
+function click3() {
+  document.getElementById("cc3");
+  cc3.select();
+  document.execCommand("copy")
+}
+function click4() {
+  document.getElementById("cc4");
+  cc4.select();
+  document.execCommand("copy")
+}
+
+function click5() {
+  document.getElementById("cc5");
+  cc5.select();
+  document.execCommand("copy")
+}
+
+function click6() {
+  document.getElementById("cc6");
+  cc6.select();
+  document.execCommand("copy")
+}
+
+function click7() {
+  document.getElementById("cc7");
+  cc7.select();
+  document.execCommand("copy")
+}
+
+function click8() {
+  document.getElementById("cc8");
+  cc8.select();
+  document.execCommand("copy")
+}
+function click9() {
+  document.getElementById("cc9");
+  cc9.select();
+  document.execCommand("copy")
+}
+
+function click10() {
+  document.getElementById("cc10");
+  cc10.select();
+  document.execCommand("copy")
+}
+
+function click11() {
+  document.getElementById("cc11");
+  cc11.select();
+  document.execCommand("copy")
+}
+
+function click12() {
+  document.getElementById("cc12");
+  cc12.select();
+  document.execCommand("copy")
+}
+
+function click13() {
+  document.getElementById("cc13");
+  cc13.select();
+  document.execCommand("copy")
+}
+
+function click14() {
+  document.getElementById("cc14");
+  cc14.select();
+  document.execCommand("copy")
+}
+
+function click15() {
+  document.getElementById("cc15");
+  cc15.select();
+  document.execCommand("copy")
+}
+
